244d12ef4fc29e14d3b69070b4c3d3ce069f4dd1
Maroš Varchola <marosvarchola@sunray.sk>
2023-12-01 00:46:13 +0100

    feat: added docstrings to datacube api

--- datacube_api.py
+++ datacube_api.py
@@ -11,10 +11,17 @@ cache_dir = './cache'
 memory = Memory(cache_dir, verbose=0)
 
 class DatacubeAPI:
+    """
+    Singleton class for accessing the Datacube API.
+
+    Attributes:
+        BASE_URL (str): Base URL of the Datacube API.
+    """
     BASE_URL = "https://data.statistics.sk/api/v2/"
     _instance = None
 
     def __new__(cls):
+        """Ensures only one instance of the class is created."""
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance.session = requests.Session()
@@ -22,10 +29,22 @@ class DatacubeAPI:
 
     @classmethod
     def get_instance(cls):
+        """
+        Returns the singleton instance of the class.
+
+        :return: Singleton instance of DatacubeAPI.
+        """
         return cls.__new__(cls)
 
     @classmethod
     def _make_request(cls, endpoint: str, params: dict = None) -> requests.Response:
+        """
+        Makes a GET request to the specified API endpoint.
+
+        :param endpoint: API endpoint to make the request to.
+        :param params: Parameters to be sent with the request.
+        :return: Response object from the requests library.
+        """
         try:
             response = cls.get_instance().session.get(f"{cls.BASE_URL}{endpoint}", params=params)
             response.raise_for_status()
@@ -40,13 +59,19 @@ class DatacubeAPI:
 
     @staticmethod
     def _parse_json_response(response: requests.Response) -> dict:
+        """
+        Parses a JSON response.
+
+        :param response: Response object to parse.
+        :return: Parsed JSON data as a dictionary.
+        """
         try:
             return response.json()
         except ValueError:
             logging.error("Invalid JSON response")
             return None
 
-    def get_table_overview(self, language='en'):
+    def get_table_overview(self, language='en') -> dict:
         """
         Retrieves an overview of all tables available in the API.
 
@@ -57,7 +82,7 @@ class DatacubeAPI:
         response = self._make_request(endpoint)
         return response.json() if response else None
 
-    def get_table_dimensions(self, cube_code, dim_code, language='en'):
+    def get_table_dimensions(self, cube_code, dim_code, language='en') -> dict:
         """
         Fetches details about specific dimensions of a table.
 
@@ -70,7 +95,18 @@ class DatacubeAPI:
         response = self._make_request(endpoint)
         return response.json() if response else None
 
-    def get_data(self, cube_code, region_code, year, indicator_code, lang='en', file_type='json'):
+    def get_data(self, cube_code, region_code, year, indicator_code, lang='en', file_type='json') -> dict:
+        """
+        Retrieves data for a given cube, region, year, and set of indicators.
+
+        :param cube_code: The code of the data cube.
+        :param region_code: The code of the region.
+        :param year: The year for which data is requested.
+        :param indicator_code: The code of the indicator.
+        :param lang: Language code for the API response.
+        :param file_type: The type of the file to fetch ('json' or 'csv').
+        :return: JSON response or CSV file content.
+        """
         endpoint = f"dataset/{cube_code}/{region_code}/{year}/{indicator_code}?lang={lang}&type={file_type}"
         response = self._make_request(endpoint)
         if response:
@@ -80,7 +116,7 @@ class DatacubeAPI:
                 return pd.read_csv(response.url)
         return None
 
-    def get_dimension_info(self, json_stat, dimension):
+    def get_dimension_info(self, json_stat, dimension) -> dict:
         """
         Extracts detailed information about a particular dimension from a dataset.
 
@@ -97,8 +133,9 @@ class DatacubeAPI:
             }
         return None
 
+
 @memory.cache
-def search_city_get_code(city_name):
+def search_city_get_code(city_name: str) -> str:
     """
     Searches for a city by name and returns its code.
 
@@ -120,7 +157,7 @@ def search_city_get_code(city_name):
 
 
 @memory.cache
-def get_latest_year():
+def get_latest_year() -> str:
     """
     Retrieves the most recent year available in the dataset.
 
@@ -136,7 +173,7 @@ def get_latest_year():
 
 
 @memory.cache
-def get_all_indicators():
+def get_all_indicators() -> dict:
     """
     Retrieves all available indicators from the dataset.
 
@@ -152,7 +189,15 @@ def get_all_indicators():
 
 
 @memory.cache
-def get_land_data(cities_string, year, indicators):
+def get_land_data(cities_string: str, year: str, indicators: list) -> dict:
+    """
+    Fetches data for each indicator for specified cities and year.
+
+    :param cities_string: Comma-separated string of city codes.
+    :param year: The year for which data is requested.
+    :param indicators: A list of indicators to fetch data for.
+    :return: A dictionary with city codes as keys and dictionaries of city names and indicator data as values.
+    """
     data = {}
     api = DatacubeAPI.get_instance()
     indicator_string_list = ','.join(indicators)
@@ -197,7 +242,13 @@ def get_land_data(cities_string, year, indicators):
     return data
 
 @memory.cache
-def get_land_data_cities_code(cities_code_list):
+def get_land_data_cities_code(cities_code_list: list) -> pd.DataFrame:
+    """
+    Fetches and formats land data for a list of city codes.
+
+    :param cities_code_list: List of city codes.
+    :return: DataFrame with land data indexed by city codes.
+    """
     if not cities_code_list:
         logging.error("City codes are required")
         return None
@@ -231,14 +282,20 @@ def get_land_data_cities_code(cities_code_list):
     return df.set_index('City Code')
 
 @memory.cache
-def get_city_codes(cities_name_list):
+def get_city_codes(cities_name_list: list) -> list:
+    """
+    Converts a list of city names to their corresponding city codes.
+
+    :param cities_name_list: List of city names.
+    :return: List of corresponding city codes.
+    """
     if not cities_name_list:
         logging.error("City names are required")
         return None
 
     # Convert cities to a list if it's a pandas DataFrame or Series
     if isinstance(cities_name_list, (pd.DataFrame, pd.Series)):
-        cities = cities_name_list.tolist()
+        cities_name_list = cities_name_list.tolist()
 
     all_city_codes = []
 
@@ -257,6 +314,14 @@ def get_city_codes(cities_name_list):
     return all_city_codes
 
 @memory.cache
-def get_land_data_cities_name(cities_name_list):
+def get_land_data_cities_name(cities_name_list: list) -> pd.DataFrame:
+    """
+    Retrieves land data for a list of city names.
+
+    :param cities_name_list: List of city names.
+    :return: DataFrame with land data indexed by city codes.
+    """
     city_codes_list = get_city_codes(cities_name_list)
+    if not city_codes_list:
+        return None
     return get_land_data_cities_code(city_codes_list)
